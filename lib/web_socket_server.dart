74fb5d78727c4f9c8f9baf39a265ae273c4daa24
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-03-24 16:48:27 +0530

    local host working. Upper case message sent back to client from server instantaneously.

---
+++ lib/web_socket_server.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'dart:async';
+
+
+
+main() {
+  runZoned(() async {
+    var server = await HttpServer.bind('127.0.0.1', 8080);
+    await for (var req in server) {
+      var socket = await WebSocketTransformer.upgrade(req);
+      socket.listen((msg){
+        print('Message received: $msg');
+        socket.add(msg.toString().toUpperCase());
+      });
+    }
+  },
+      onError: (e) => print("An error occurred."));
+}
