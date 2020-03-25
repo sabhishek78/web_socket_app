8a50272523fc7f2fd9e8e1bbee2140a6289aa61d
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-03-25 15:21:17 +0530

    broadcast message successfully sent to all

--- lib/web_socket_server.dart
+++ lib/web_socket_server.dart
@@ -1,20 +1,34 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
 
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 
+//this code receives message from client and sends it back instantly and aftr 2 seconds after converting to uppercase
 main() {
+  List<WebSocket>connections=[];
+  String message;
   runZoned(() async {
     var server = await HttpServer.bind('127.0.0.1', 8080);
+    print("Websocket started . Listening on port 8080");
     await for (var req in server) {
+      print('request for connection received');
       var socket = await WebSocketTransformer.upgrade(req);
+      connections.add(socket);
+      print(connections.length);
       socket.listen((msg){
+        message=msg;
         print('Message received: $msg');
-        socket.add(msg.toString().toUpperCase());
-        sleep(Duration(seconds:2));
-        socket.add((msg.toString()+"Again").toUpperCase());
+        if(msg!=null)for(int i=0;i<connections.length;i++){ //send to every client
+        connections[i].add(message.toUpperCase());
+        }
+        Future.delayed(Duration(milliseconds: 2000),(){socket.add((msg.toString()+"Again").toUpperCase());});
+
       });
+
+
     }
-  },
+    },
       onError: (e) => print("An error occurred."));
 }
