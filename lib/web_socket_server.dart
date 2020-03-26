21c0479fa1d6b7bc77b61982fdd459814e4cc9f6
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-03-26 15:41:03 +0530

    File I/O finished. messages are being stored in a file on the PC. When a new client joins he receives the past messages.

--- lib/web_socket_server.dart
+++ lib/web_socket_server.dart
@@ -9,30 +9,71 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 main() {
   List<WebSocket>connections=[];
   List<String>uripathList=[];
+//  List<String> messagesList=[];
+
+  File file = new File("G:\messages.txt");
   String message;
   runZoned(() async {
     var server = await HttpServer.bind('127.0.0.1', 8080);
     print("Websocket started . Listening on port 8080");
     await for (var req in server) {
-      print('request for connection received');
+     print('request for connection received');
       var socket = await WebSocketTransformer.upgrade(req);
       connections.add(socket);
       uripathList.add((req.uri.path).toString());
+      file.readAsString().then((String contents) {
+        print("sending file to new connection");
+        String temp="";//send file to new connection
+        if(contents.length!=0){
+          for(int i=0;i<contents.length;i++){
+            print("browsing through contents");
+            if(contents[i]!='\n'){
+              print("contents[i=${contents[i]}");
+              temp=temp+contents[i];
+              print("temp=$temp");
+            }
+            if(contents[i]=='\n'){
+              print("sending $temp to socket");
+              socket.add(temp);
+              temp='';
+            }
 
+          }
+        }
+
+       // socket.add(contents);
+      });
       socket.listen((msg){
-        message=msg;
-        print('Message received: $msg');
-        if(msg!=null)for(int i=0;i<connections.length;i++){
+       message=msg;
+      //  messagesList.add(msg);
+       print('Message received: $msg');
+        writeToFile(file,msg);
+        print("Printing file contents...");
+        printFile(file);
+        if(msg!=null)for(int i=0;i<connections.length;i++){ //send message to all except the sender
           if(connections[i]!=socket && uripathList[i]==(req.uri.path).toString())
         connections[i].add(message.toUpperCase());
+//          file.readAsString().then((String contents) {
+//            connections[i].add(contents);
+//          });
         }
         //Future.delayed(Duration(milliseconds: 2000),(){socket.add((msg.toString()+"Again").toUpperCase());});
       });
     }
     },
-      onError: (e) => print("An error occurred."));
+      onError: (e) => (){}//print("An error occurred.")
+  );
 }
-
+writeToFile(File file,String msg)async{
+ print("writing to file");
+  await file.writeAsString(msg+'\n', mode: FileMode.append);
+  print("successfully written on file");
+}
+void printFile(File file){
+  file.readAsString().then((String contents) {
+    print(contents);
+  });
+}
 //main() {
 //  List<WebSocket>connections=[];
 //  String message;
